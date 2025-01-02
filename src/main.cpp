1ddc4f814993f26471e388dc7448da3ceeec9caf
Kenta Kato <kenta.kato.4321@gmail.com>
2025-01-03 03:06:06 +0900

    chore: Add Eigen3 library and MPPIController class for robot control

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+#include <cstddef>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <array>
+#include <Eigen/Dense>
+#include <random>
 
 
 struct Rectangle {
@@ -46,16 +50,16 @@ struct Rectangle {
 
 class DiffDriveRobot {
 public:
-    DiffDriveRobot(const std::array<double, 2> & center, const double init_yaw = 0.0, const double radius = 10.0)
-    : radius_(radius), center_(center), yaw_(init_yaw)
+    DiffDriveRobot(const std::array<double, 2> & center, const double init_theta = 0.0, const double radius = 10.0)
+    : radius_(radius), center_(center), theta_(init_theta)
     {
     }
 
     void move(const double v, const double omega, const double dt)
     {
-        center_[0] += v * std::cos(yaw_) * dt;
-        center_[1] += v * std::sin(yaw_) * dt;
-        yaw_ += omega * dt;
+        center_[0] += v * std::cos(theta_) * dt;
+        center_[1] += v * std::sin(theta_) * dt;
+        theta_ += omega * dt;
     }
 
     void draw(cv::Mat& image) const
@@ -70,8 +74,8 @@ public:
             cv::LINE_AA);
 
         // draw the orientation
-        const double dx = radius_ * std::cos(yaw_);
-        const double dy = radius_ * std::sin(yaw_);
+        const double dx = radius_ * std::cos(theta_);
+        const double dy = radius_ * std::sin(theta_);
         cv::line(
             image,
             center_cv,
@@ -87,7 +91,112 @@ private:
     const cv::Scalar orientation_color_ = cv::Scalar(0, 0, 0);
 
     std::array<double, 2> center_;
-    double yaw_;
+    double theta_;
+};
+
+
+template <int Dim>
+Eigen::MatrixXd sample_normal_dist_multiple(
+    const Eigen::Matrix<double, Dim, Eigen::Dynamic> & mean /* dim  x num_samples */,
+    const Eigen::Matrix<double, Dim, Dim> & cov)
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    const size_t num_samples = mean.cols();
+
+    //  L is a lower triangular matrix (Cholesky decomposition)
+    //  cov = L * L^T
+    Eigen::LLT<Eigen::MatrixXd> cholesky_solver(cov);
+    Eigen::MatrixXd L = cholesky_solver.matrixL(); // d×d
+
+    std::normal_distribution<> dist(0.0, 1.0);
+
+    Eigen::MatrixXd Z(Dim, num_samples);
+    for(int i = 0; i < Dim; ++i) {
+        for(int j = 0; j < num_samples; ++j) {
+            Z(i, j) = dist(gen);
+        }
+    }
+
+    Eigen::MatrixXd X = mean + L * Z; // dim × num_samples
+    return X;
+}
+
+class MPPIController {
+public:
+    // alias
+    using State = Eigen::Vector3d;
+
+public:
+    MPPIController(const State & init_state, const size_t horizon_length, const size_t num_samples)
+    : horizon_length_(horizon_length), num_samples_(num_samples), current_state_(init_state)
+    {
+        noise_cov_ = Eigen::Matrix2d::Identity();
+        noise_cov_(0, 0) = 0.1;
+        noise_cov_(1, 1) = 0.1;
+
+        last_inputs_ = Eigen::MatrixXd::Zero(2, horizon_length);
+        for (int i = 0; i < horizon_length; ++i) {
+            last_inputs_(0, i) = 10.0;
+        }
+    }
+
+    void control(cv::Mat & image)
+    {
+        for (size_t i = 0; i < num_samples_; ++i) {
+            const auto next_inputs = sample_normal_dist_multiple<2>(last_inputs_, noise_cov_);
+            // TODO: clamp the inputs
+
+            const auto next_states = simulate(next_inputs, 0.1);
+
+            const cv::Scalar gray{100, 100, 100};
+            for (int i = 0; i < horizon_length_; ++i) {
+                const auto state = next_states.col(i);
+                const auto center = cv::Point(cvRound(state[0]), cvRound(state[1]));
+                cv::circle(image, center, 1, gray, cv::FILLED, cv::LINE_AA);
+            }
+        }
+    }
+
+
+private:
+    const size_t horizon_length_;
+    const size_t num_samples_;
+    State current_state_;
+    Eigen::Matrix2d noise_cov_;
+
+    Eigen::MatrixXd last_inputs_;
+
+
+    State dynamics(const State & state, const double v, const double omega, const double dt)
+    {
+        State next_state;
+        next_state[0] = state[0] + v * std::cos(state[2]) * dt;
+        next_state[1] = state[1] + v * std::sin(state[2]) * dt;
+        next_state[2] = state[2] + omega * dt;
+        return next_state;
+    }
+
+    Eigen::Matrix<double, 3, Eigen::Dynamic> simulate(const Eigen::Matrix<double, 2, Eigen::Dynamic> & inputs, const double dt)
+    {
+        if (inputs.cols() != horizon_length_) {
+            throw std::runtime_error("The number of columns of inputs must be equal to the horizon length.");
+        }
+
+        Eigen::Matrix<double, 3, Eigen::Dynamic> states(3, horizon_length_);
+        states.col(0) = current_state_;
+
+        for (size_t i = 1; i < horizon_length_; ++i) {
+            const auto prev_state = states.col(i - 1);
+            const auto input = inputs.col(i);
+            states.col(i) = dynamics(prev_state, input[0], input[1], dt);
+        }
+
+        return states;
+    }
+
+
 };
 
 
@@ -109,9 +218,13 @@ int main()
         }
     };
 
-    DiffDriveRobot robot{{400, 400}};
+    const std::array<double, 2> init_robot_pos{100., 100.};
+    const double init_robot_theta = 0.0;
+    DiffDriveRobot robot{init_robot_pos, init_robot_theta};
     robot.draw(img);
 
+
+#if 0
     const int dt_ms = 10;
     const int ESC_KEY = 27;
     for (int i = 0; i < 1000; ++i)
@@ -130,6 +243,17 @@ int main()
             break;
         }
     }
+#else
+    MPPIController controller{Eigen::Vector3d{init_robot_pos[0], init_robot_pos[1], init_robot_theta}, 100, 100};
+    for (int i = 0; i < 100; ++i)
+    {
+        draw_background();
+        controller.control(img);
+        cv::imshow("Robot", img);
+        std::cout << "Press any key to continue..." << std::endl;
+        cv::waitKey(0);
+    }
+#endif
 
     return EXIT_SUCCESS;
 }
